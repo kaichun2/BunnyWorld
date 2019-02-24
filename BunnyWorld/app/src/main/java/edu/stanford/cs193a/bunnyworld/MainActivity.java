9774bdfd4933c56132cc2fdb6f87813db108755f
Luis Ulloa <ulloa@stanford.edu>
2019-02-24 12:34:03 -0800

    starter files

---
+++ BunnyWorld/app/src/main/java/edu/stanford/cs193a/bunnyworld/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.stanford.cs193a.bunnyworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
