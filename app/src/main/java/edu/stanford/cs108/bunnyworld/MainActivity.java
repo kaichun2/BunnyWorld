df0170c9dd04a55bcdd25fc54508b4f49f6103e9
Ip Chun Chan <37690196+kaichun2@users.noreply.github.com>
2019-03-02 04:31:25 -0800

    Update MainActivity.java

--- app/src/main/java/edu/stanford/cs108/bunnyworld/MainActivity.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/MainActivity.java
@@ -2,6 +2,8 @@ package edu.stanford.cs108.bunnyworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] pageArray = {
+                "Page 2",
+                "Page 3"
+        };
+
+        Spinner gamePage = (Spinner) findViewById(R.id.gamePage);
+        SpinnerAdapter gamePage_adapter = new ArrayAdapter<String>( this,R.layout.spinner_item,pageArray);
+        gamePage.setAdapter(gamePage_adapter);
+    }
+
+    protected void startGame(View view) {
+        Spinner gamePage = (Spinner) findViewById(R.id.gamePage);
+        String pageNum = gamePage.getSelectedItem().toString();
+        // To do: change to pageNum
+
+    }
+
+    protected void editGame(View view) {
+        Spinner gamePage = (Spinner) findViewById(R.id.gamePage);
+        String pageNum = gamePage.getSelectedItem().toString();
+        // To do: change to pageNum
     }
 }
